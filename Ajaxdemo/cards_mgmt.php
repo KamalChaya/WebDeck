025b9fce56c5d7be84cdfe43284f31dc7458f4dc
Jackson Carter <cartejac@flip1.engr.oregonstate.edu>
2014-02-09 18:11:42 -0800

    Added network_class interface

--- Ajaxdemo/cards_mgmt.php
+++ Ajaxdemo/cards_mgmt.php
@@ -8,19 +8,23 @@
 	
 	switch($op){
 		case 0: 	//Create the card
-			create_card();
+			create_card(); // <-- Deprecated
 			break;
 		
-		case 1: 	//Move the card
+		case 1: 	//Move the card: updates x and y coordinates
 			move_card();
 			break;
 		
-		case 2:	//Get positions
+		case 2:	//Get positions: retrieve positions since last query
 			get_positions();
 			break;
 		
 		case 3:	//Get all of the cards for a session.
 			enter_session();
+			break;
+			
+		case 4:	//Get a lock on selection
+			secure_lock();
 			break;
 			
 		default:
@@ -98,5 +102,26 @@
 		$result = execute_query($sql);	
 		$tableString = php_entity_encode($result);
 		echo $tableString;
+	}
+	
+	//lock = -1 means the card is unlocked
+	function secure_lock()
+	{
+		$game_id = $_POST['game_id'];
+		$card_id = $_POST['card_id'];
+		$user_id = $_POST['user_id'];
+		
+		$sql = "	UPDATE Cards C
+				SET C.lock = $user_id
+				WHERE C.lock = -1 AND C.game = $game_id AND C.cid = $card_id;";
+				
+		$result = execute_query($sql);
+		if($result){
+			echo "They got the lock!";
+			
+		} else {
+			echo "Somebody has it locked!";
+			
+		}
 	}
 ?>
---
+++ network_class.js
@@ -0,0 +1,12 @@
+
+function mk_network()
+{
+	this.game_id
+	
+	//Member functions
+	update_board()//Get x,y positions and flip value
+	set_pos(card_id)//set the x,y position
+	select()//
+	secure_lock()//
+	flipdb(card_id, flip_val) //Sets "flipped" to the new flip_val
+}
