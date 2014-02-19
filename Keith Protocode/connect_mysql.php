adc1e5181e1ba830d20a8c786ea37235d0b31922
changjo512 <jonathan.chang512@gmail.com>
2014-02-18 19:51:20 -0800

    Final Cleanup
    Final deletion of duplicate and superfluous files.

--- Keith Protocode/connect_mysql.php
+++
@@ -1,9 +0,0 @@
-<?php
-function connect_mysql(){
-	$con = mysqli_connect('database_location', 'user', 'password', 'databasename'); 
-	if (mysqli_connect_errno($con)){
-	echo "Failed to connect to MySQL: " . mysqli_connect_errno();
-	}
-	return $con;
-}
-?>
