9645fe6ab222427ec646dc4786dedd3c123cbb7a
Jackson Carter <cartejac@flip2.engr.oregonstate.edu>
2014-02-23 13:16:11 -0800

    Shell script modifications

--- change_perms.sh
+++ change_perms.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-cd ..
+cd ~/public_html
 find ./ -type d -exec chmod 755 {} \;
 find ./ -type f -exec chmod 644 {} \;
-cd WebDeck
-chmod 0777 change_perms.sh
+cd ~/public_html/WebDeck
+chmod 0700 change_perms.sh
