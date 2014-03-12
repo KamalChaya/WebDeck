9565604e614b227f4e08859e19e0d722883b39f9
alltucki <alltucki@onid.orst.edu>
2014-03-11 21:05:46 -0700

    Network Class Documentation
    Added comment headers to all functions in network_class.js

--- network_class.js
+++ network_class.js
@@ -17,19 +17,16 @@ function mk_network()
 {
 	var locations = document.URL.split("/");
 	this.web_root = document.URL.replace(locations[locations.length - 1], "");
-	this.game_id = 4;
+	this.game_id = -1;
 	this.board_update_interval = 500;	//milliseconds between updates
-	this.board_update_timer; 			//A reference to the update timer (incase we need to remove it)
-	//this.last_update;					//Seconds since the EPOCH that we last updated =)	
-	this.send_update_interval = 300;		//milliseconds between updating a card.
-	this.send_update_timer;			//A reference to the send timer (we will need to remove it)
+	this.board_update_timer; 			//A reference to the update timer (in case we need to remove it)
+	this.send_update_interval = 300;	//milliseconds between updating a card.
+	this.send_update_timer;				//A reference to the send timer (we will need to remove it)
 	//Member functions
 	//Responsible for getting the 52 cards from the server, creating them
 		//and making them draggable.
 	this.init_board = function()
 	{
-		//alert("Initializing board through network class");
-		
 		this.change_game(localStorage["game_id"]);
 		localStorage.removeItem("game_id");
 
@@ -48,7 +45,8 @@ function mk_network()
 		var userid = this.ajax('cards_mgmt.php', namequery, err_funct, false);
 		userid = JSON.parse(userid.responseText);
 
-		if(!jQuery.isEmptyObject(userid)) {
+		//Get the user's id, or assign one if it does not yet exist
+		if (!jQuery.isEmptyObject(userid)) {
 			player.player_id = userid[0].id;
 			player.username = username;
 			console.log("Found user " + player.username + " with id " + player.player_id);
@@ -75,17 +73,23 @@ function mk_network()
 		
 		//Make the cards
 		var cur_card;
-		for(card_idx in new_game.result){
+		for (card_idx in new_game.result){
 			cur_card = new_game.result[card_idx];
 			var new_card = new card(cur_card.cid);
 
 			network.set_card_attr(cur_card);
 		}
 
-		//board_update_timer = setInterval("network.begin_board_update()", this.board_update_interval);
 		board_update_timer = setInterval(function(){network.begin_board_update()}, this.board_update_interval);
 	}
 
+	//	Timeout_games:
+	//	Calls cards_mgmt.php and obtains a list of games with updates older than two days.
+	//	Then, loops through the return array and asks cards_mgmt.php to delete it
+	//	from the database.
+	//	Preconditions: N/A
+	//	Postconditions: All games whose last update was more than two days ago are
+	//	deleted from the database, both their cards and the game data
 	this.timeout_games = function()
 	{
 		var var_string = 'op=15';
@@ -93,26 +97,41 @@ function mk_network()
 		var return_text = JSON.parse(ajax_obj.responseText);
 		console.log(return_text);
 
-		for(i in return_text) {
+		for (i in return_text) {
 			var_string = 'op=16&game_id=' + return_text[i].game_id;
 			ajax_obj = this.ajax('cards_mgmt.php', var_string, err_funct, false);
 		}
 	}
 	
+	//	Reset_cards:
+	//	A function defined simply so the cards can be reset through a button.
+	//	Calls cards_mgmt.php and gives it the current game_id, as well as
+	//	the approrpiate operation code.
+	//	Preconditions: N/A
+	//	Postconditions: All cards for the given game_id are set to (0,0) and
+	//	given a random z-index
 	this.reset_cards = function()
 	{
 		var var_string = 'op=19&game_id=' + this.game_id;
 		var ajax_obj = this.ajax('cards_mgmt.php', var_string, err_funct, false);
 	}
 	
+	//	begin_board_update:
+	//	The beginning of the networking. Calls cards_mgmt.php, and directs
+	//	the result to finish_board_update.
+	//	Preconditions: A valid network connection is crucial
+	//	Postcondiitons: Update information is sent to finish_board_update 
 	this.begin_board_update = function()
 	{
-		//console.log("last_update preceeding update: " + last_update);
 		var var_string = 'op=2&game_id=' + this.game_id + "&lastu=" + last_update;
 		this.ajax('cards_mgmt.php', var_string, this.finish_board_update, true);
 	}
 	
-	//Get x,y positions and flip value
+	//	finish_board_update:
+	//	Parses the returned information about cards and sets the client's
+	//	local cards appropriately.
+	//	Preconditions: begin_board_update is run, and the ajax object is passed
+	//	Postconditions: Local cards are updated 
 	this.finish_board_update = function(ajax_obj)
 	{
 		try {
@@ -121,10 +140,7 @@ function mk_network()
 			console.log("Parsing error:", e);
 		}
 		last_update = board_state.time;
-		//console.log("last_update following update: " + last_update);
 		var added_card = 0;
-
-		//console.log(board_state.result);
 
 		//Set positions, flipped and locks.
 		//Handle cards in other people's hands
@@ -135,8 +151,10 @@ function mk_network()
 		}
 	}
 	
-	//Will set all visual and functional components of a card including:
-	//border, position, z_index, "inhandedness", flip value
+	//	set_card_attr:
+	//	Updates the attributes of a card
+	//	Preconditions: A template card with different attributes is given
+	//	Postconditions: The specific local card is updated 
 	this.set_card_attr = function(cur_card)
 	{
 		if (cur_card.in_hand == player.player_id) {
@@ -176,6 +194,14 @@ function mk_network()
 		}
 	}
 	
+	//	change_game:
+	//	Loads up a new game session. Contains checks for null game_ids,
+	//	but does not check for non-alphanumeric characters. If no game session with that id
+	//	is found in the database, the program asks the user for a short description to be used
+	//	in the lobby.
+	//	Preconditions: A new game id is passed
+	//	Postconditions: If no game exists with the given id, one is created with the given description.
+	//	Appropriate info is sent to the database, and information regarding the cards is sent to the client.
 	this.change_game = function(new_game)
 	{
 		if(typeof new_game != 'undefined' && new_game != "") {
@@ -185,7 +211,6 @@ function mk_network()
 		var num_cards = this.ajax('cards_mgmt.php', game_query, err_funct, false);
 		num_cards = JSON.parse(num_cards.responseText);
 		num_cards = num_cards[0].num_cards;
-		//console.log(num_cards);
 
 		if (num_cards == 52){
 			//console.log("have 52 cards");
@@ -209,9 +234,10 @@ function mk_network()
 		}
 	}
 
-	
-	//This small internal function will handle getting
-	//	and sending card positions.
+	//	_send_pos:
+	//	Sends the position of a card	
+	//	Preconditions: N/A 
+	//	Postconditions: The info is sent through cards_mgmt.php
 	this._send_pos = function(card_idx)
 	{
 		console.log("_send_pos has been called: transmitting card position: " + card_idx);
@@ -223,32 +249,38 @@ function mk_network()
 		//AJAX
 		var var_string = "op=1&game_id=" + this.game_id + "&cid=" + card_idx + "&pid=" + player.player_id + "&x_pos=" + x_pos + "&y_pos=" + y_pos + "&z_pos=" + z_pos;
 		var ajax_obj = this.ajax("cards_mgmt.php", var_string, empty_funct, true);
-		//console.log(ajax_obj.responseText);
 	}
 	
-	//Stop transmitting the current position of the card.
+	//	stop_send:
+	//	Stops transmitting	
+	//	Preconditions: N/A 
+	//	Postconditions: The update interval is cleared
 	this.stop_send = function(card_idx)
 	{
 		clearInterval(this.send_update_timer);
 	}
 	
+	//	flipdb:
+	//	Gets information about flip status from the database
+	//	Preconditions: A valid card id is passed in
+	//	Postconditions: The flip status is returned
 	this.flipdb = function(card_idx, flip_val)
 	{
 		var var_string = "op=6&pid="+ player.player_id +"&card_id=" + card_idx + "&game_id=" + this.game_id + "&flipped=" + flip_val;
-		//alert(var_string);
 		var ajax_obj = this.ajax("cards_mgmt.php", var_string, err_funct, false);
-		//alert(ajax_obj.responseText);
 		
 		try {
 			var board_state = JSON.parse(ajax_obj.responseText);
 		} catch (e) {
 			console.log("Parsing error:", e);
 		}
-		//alert(out_str);
 		return board_state;
-	}	//Sets "flipped" to the new flip_val
+	}
 	
-	
+	//	st_add_hand_db:
+	//	Puts a card in a player's hand
+	//	Preconditions: The card is valid to be placed in the player's hand
+	//	Postconditions: The card is placed in the player's hand
 	this.st_add_hand_db = function(card_idx)
 	{
 		var var_string = 'op=9&game_id=' + this.game_id + '&card_id=' + card_idx + "&player_id=" + player.player_id;
@@ -285,31 +317,28 @@ function mk_network()
 		return ret_val.result;
 	}
 	
-	//Responsible for carrying out AJAX requests
 	//async: true or false for asynchronous requests. non-async requests return an AJAX obj
+	//	ajax:
+	//	Executes an ajax query
+	//	Preconditions: A valid query is passed in
+	//	Postconditions: The ajax object associated with the return value of a query is returned.
 	this.ajax = function(file, var_string, funct, async){
-		//alert('Ajax accessed');
-		if(window.XMLHttpRequest){
+		if (window.XMLHttpRequest) {
 			var ajax_obj =  new XMLHttpRequest();
-		}else{
+		} else {
 			var ajax_obj = new ActiveXObject("Microsoft.XMLHTTP");
 		}
 		
 		ajax_obj.open('POST', this.web_root + file, async);
 		ajax_obj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		
-		//alert(var_string);
+
 		ajax_obj.send(var_string);
 		
 		if (async){
-			//alert("Registered async");
 			ajax_obj.onreadystatechange = function(){
-				if(ajax_obj.readyState == 4 && ajax_obj.status == 200){
+				if (ajax_obj.readyState == 4 && ajax_obj.status == 200) {
 					funct(ajax_obj);
-					//return ajax_obj;
-					//return obj;
-				
-				}else{
+				} else {
 					//alert('Ready State: ' + ajax_obj.readyState + " Status: " + ajax_obj.status);
 				}
 			}
@@ -317,7 +346,4 @@ function mk_network()
 			return ajax_obj;
 		}
 	}
-	
-	//Constructor
-	
 }
